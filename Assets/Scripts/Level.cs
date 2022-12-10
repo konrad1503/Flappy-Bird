59b7067631ceea0e233b2b9a4ae1907c51945309
konrad1503 <fresher002@gmail.com>
2022-12-10 18:46:03 +0100

    CreateGapPipes
    Created void for creating gap pipes, preparing for tracking them, which one is present on the screen.

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -7,11 +7,27 @@ public class Level : MonoBehaviour
 	private const float CAMERA_OTROGRAPHIC_SIZE = 50f;
 	private const float PIPE_WIDTH = 7.8f;
 	private const float PIPE_HEAD_HEIGHT = 3.8f;
+	private List<Transform> pipeList;
+/*
+	private void Awake() 
+	{
+		pipeList = new List<Transform>();
+	}
+*/
 	private void Start() {
 		CreatePipe(40f, 20f, true);
 		CreatePipe(40f, 20f, false);
-		
+		CreateGapPipes(50f, 10f, 40f);
 	}
+
+	private void CreateGapPipes(float GapY, float gapSize, float xPosition)
+	{
+		CreatePipe(GapY - gapSize * .5f, xPosition, true);
+		CreatePipe(CAMERA_OTROGRAPHIC_SIZE * 2f - GapY - gapSize * .5f, xPosition, false);
+
+	}
+
+
 
 	private void CreatePipe(float height, float xPosition, bool createBottom)
 	{
