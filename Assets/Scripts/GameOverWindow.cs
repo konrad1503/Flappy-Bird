f0b398937df44ef5f3d67a1d14b0765f91a5f11c
konrad1503 <fresher002@gmail.com>
2022-12-19 09:06:05 +0100

    Game Over Window

---
+++ Assets/Scripts/GameOverWindow.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverWindow : MonoBehaviour
+{
+    private Text scoreText;
+
+    private void Awake() {
+        scoreText = transform.Find("scoreText").GetComponent<Text>();
+
+        transform.Find("retryBtn").GetComponent<Button>().onClick.AddListener(TaskOnClick);
+        Hide();
+    }
+
+    private static void TaskOnClick() {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    private void Start() {
+        Bird.GetInstance().OnDied += Bird_OnDied;
+    }
+
+    private void Bird_OnDied(object sender, System.EventArgs e) {
+        scoreText.text = Level.GetInstance().GetPipesPassedCount().ToString();
+        Show();
+    }
+
+    private void Hide() {
+        // gameObject.SetActive(false);
+        gameObject.transform.localScale = new Vector3(0, 0, 0);
+    }
+
+    private void Show() {
+        // gameObject.SetActive(true);
+        gameObject.transform.localScale = new Vector3(1, 1, 1);
+    }
+}
